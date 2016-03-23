e832ddbc9c09277e24ac529c1d1e2db189ba3242
Matt <Matt@BladePro>
2016-03-23 17:04:51 -0500

    mmonical_rot13Arrays

--- mmonical_ROT13Arrays/src/mmonical_rot13arrays/Mmonical_ROT13Arrays.java
+++ mmonical_ROT13Arrays/src/mmonical_rot13arrays/Mmonical_ROT13Arrays.java
@@ -12,150 +12,133 @@ import java.util.Scanner;
 public class Mmonical_ROT13Arrays {
 
     /**
-     * @param args the command line arguments
      */
     public static Scanner get = new Scanner (System.in);
     public static void main(String[] args) {
-        // TODO code application logic here
-    //Name: Michael J Monical
-//Class: 1400-003
-//Program #: 6 
-//Due Date: February 24th, 2016
-//Colleagues : None
-
-       
-
-
-        char ch;
-        int a;
-        boolean boolflag;
-        int counter = 0;
-        System.out.print ("Enter a sentence to ROT13: ");
-        String sentence = get.nextLine();
-        sentence = sentence.toUpperCase();
+      
+        String[] sentences = new String[5];
+        getSentences(sentences);
+        displayOriginal(sentences);
+        displayROT13(sentences);
+        displayCombinedROT13(sentences);
         
-        for (a = 0; a < sentence.length(); a++){
-            ch = sentence.charAt(a);
-            int hexChar = (int) ch;
-            if (hexChar == 0x20)
-                System.out.print(" ");
-            if (hexChar < 0x41){
-                continue;
-            }
-            else if (hexChar > 0x5A){
-                continue;
-            }
-            hexChar = (hexChar + 0x0D) % 0x5B;
-            if (hexChar < 0x41)
-                hexChar = hexChar + 0x41;
-            System.out.print((char)hexChar);
+    }
+    /**
+     * This method allows the user to enter text into each of the
+     * element of the String array that it receives.
+     * @param sentences     An array of String[] data
+     */
+    public static void getSentences(String[] sentences){
+        System.out.println("Enter your 5 sentences below:");
+        System.out.println("");
+        for (int x = 0; x < sentences.length; x++){
+            System.out.printf("Sentence %d> ",x+1);
+            sentences[x] = get.nextLine();
         }
         System.out.println("");
-        a = 0;
-        while ( a != sentence.length() )
-        {
-            ch = sentence.charAt(a++);
-            boolflag = false;
-            
-            switch (ch)
-            {
-                case 'A':
-                    System.out.printf("N");
-                    break;
-
-                case 'B':
-                    System.out.printf("O");
-                    break;
-                case 'C':
-                    System.out.printf("P");
-                    break;
-                case 'D':
-                    System.out.printf("Q");
-                    break;
-                case 'E':
-                    System.out.printf("R");
-                    break;
-                case 'F':
-                    System.out.printf("S");
-                    break;
-                case 'G':
-                    System.out.printf("T");
-                    break;
-                case 'H':
-                    System.out.printf("U");
-                    break;
-                case 'I':
-                    System.out.printf("V");
-                    break;
-                case 'J':
-                    System.out.printf("W");
-                    break;
-                case 'K':
-                    System.out.printf("X");
-                    break;
-                case 'L':
-                    System.out.printf("Y");
-                    break;
-                case 'M':
-                    System.out.printf("Z");
-                    break;
-                case 'N':
-                    System.out.printf("A");
-                    break;
-                case 'O':
-                    System.out.printf("B");
-                    break;
-                case 'P':
-                    System.out.printf("C");
-                    break;
-                case 'Q':
-                    System.out.printf("D");
-                    break;
-                case 'R':
-                    System.out.printf("E");
-                    break;
-                case 'S':
-                    System.out.printf("F");
-                    break;
-                case 'T':
-                    System.out.printf("G");
-                    break;
-                case 'U':
-                    System.out.printf("H");
-                    break;
-                case 'V':
-                    System.out.printf("I");
-                    break;
-                case 'W':
-                    System.out.printf("J");
-                    break;
-                case 'X':
-                    System.out.printf("K");
-                    break;
-                case 'Y':
-                    System.out.printf("L");
-                    break;
-                case 'Z':
-                    System.out.printf("M");
-                    break;
-                default:
-                    System.out.printf("");
-                    boolflag = true;
-                    break;
-            }
-        if (!boolflag)
-        {
-            counter++;
-            if (counter == 5)
-            {
-                System.out.printf(" ");
-                counter = 0;
+    }
+    /**
+     * This method displays all of the elements of the array of
+     * String data that it receives, line by line (element by element).
+     * @param sentences     An Array of String[] data
+     */
+    public static void displayOriginal(String[] sentences){
+        System.out.println("The original text:");
+        for (String sentence : sentences) {
+            System.out.println(sentence);
+        }
+        System.out.println("");
+    }
+    /**
+     * This method will take one char value as a parameter and convert
+     * it to its appropriate ROT13 equivalent. The return value will be the
+     * new ROT13 char equivalent.
+     * 
+     * This method will not do any output.
+     * @param   toConvert     A character to convert as a char
+     * @return  The new ROT13 equivalent value as a char
+     */
+    public static char charConvert(char toConvert){
+        
+        int decChar = (int) toConvert;
+        if (decChar < 123){
+            if (decChar > 94){
+                decChar = decChar - 32;
             }
         }
-            
+        decChar = (decChar + 13) % 91;
+        if (decChar < 64)
+            decChar = decChar + 65;
+        return (char) decChar;
+    }
+    
+    /**
+     * This method will do the actual conversion of the String of data to its
+     * ROT13 equivalent in 5-character chunks of data. It should call on
+     * the charConvert() method to do the actual character conversion for each
+     * individual character. In other words, individual character conversion
+     * should not happen within this method.
+     * 
+     * This method will not do any output.
+     * 
+     * @param   sentence    A String variable to convert
+     * @return  The 5-characters in a group ROT13 result as a String
+     */
+    public static String convertSentence(String sentence){
+        char c;
+        int decChar;
+        String newSentence = "";
+        for (int x = 0; x < sentence.length(); x++){
+            c = sentence.charAt(x);
+            decChar = (int) c;
+            if (decChar < 123){
+                if (decChar > 94){
+                    decChar = decChar - 32;
+                }
+            }
+            if (decChar > 64){
+                if (decChar < 91){
+                    newSentence = newSentence + charConvert(c);
+                    if (newSentence.length() % 5 == 0){
+                        newSentence = newSentence + " ";
+                    }
+                }
+            }
         }
-        System.out.println("\n");
-
-
+        return newSentence;
+    }
+    
+    /**
+     * This method will display in ROT13 format all of the elements of the
+     * array of String data that it receives. It will need to call on the
+     * method convertSentence() to convert each string before it display it.
+     * Note that the original array should not be modified with ROT13 data.
+     * 
+     * @param   sentences   An array of String[] data
+     */
+    public static void displayROT13(String[] sentences){
+        System.out.println("The line-by-line ROT13:");
+        for (String sentence : sentences){
+            System.out.println(convertSentence(sentence));
+        }
+        System.out.println("");
+    }
+    /**
+     * This method takes an array of String data and combines all this Strings
+     * into a single String that is then processed by the convertSentence()
+     * method. The method should essentially display the same results as the
+     * displayROT13() method, except that there won't be separate lines of
+     * output but rather one large result instead.
+     * 
+     * @param   sentences   An array of String[] data
+     */
+    public static void displayCombinedROT13(String[] sentences){
+        String newSentence = "";
+        System.out.println("The combined ROT13");
+        for (String sentence : sentences){
+            newSentence = newSentence + sentence;
+        }
+        
+        System.out.println(convertSentence(newSentence));
     }
 }
